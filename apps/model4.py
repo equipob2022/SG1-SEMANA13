786c1d71d7003156cc1661107ae042635a5ab165
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-24 11:38:08 -0500

    Update model4.py

--- apps/model4.py
+++ apps/model4.py
@@ -16,7 +16,7 @@ def app():
     yf.pdr_override()
     #start = '2004-08-18'
     #end = '2022-01-20'
-    start = st.date_input('Start' , value=pd.to_datetime('2004-08-18'))
+    start = st.date_input('Start' , value=pd.to_datetime('2022-12-01'))
     end = st.date_input('End' , value=pd.to_datetime('today'))
 
     st.title('Predicción de tendencia de acciones')
